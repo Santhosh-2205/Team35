b6cec259105247a32d6025c15240354bb3ea5dc7
Pratham <pratham.pattanayak@students.iiit.ac.in>
2024-04-27 21:22:14 +0530

    added example form

--- code/webpage/script.js
+++ code/webpage/script.js
@@ -190,6 +190,18 @@ function generateMainAreaHTML(expandedHeight, num_expands) {
               display: block;
               padding: 10px;
             }
+            .textonly-box{
+                 width: 440px;
+              height: 60px;
+              border-radius: 12px;
+              resize: both;
+              overflow: hidden;
+              position: absolute;
+              transform: translate(-50%, -50%);
+              display: block;
+              padding: 10px;
+              border:none;
+            }
             
             .mcq-box, .single-box {
               width: 440px;
@@ -200,6 +212,15 @@ function generateMainAreaHTML(expandedHeight, num_expands) {
               transform: translate(-50%, -50%);
               display: block;
               padding: 10px;
+            }
+            .textarea-box textarea,
+            .textonly-box textarea {
+                width: 100%;
+                height: 92%;
+                border: 1px solid black;
+                font-size: 16px; /* Increase font size */
+                font-weight: bold; /* Make text bold */
+                padding: 5px; /* Adjust padding as needed */
             }
             
             textarea {
